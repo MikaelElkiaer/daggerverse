dec91d3ba210ee4cc83173904b9b6be72113dfff
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-05-31 19:18:39 +0200

    helm: Refactor

--- helm.go
+++ helm.go
@@ -2,151 +2,232 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"strings"
 )
 
-var (
-	workDir      = "/src"
-	templatedDir = "/src/templated"
+const (
+	PACKAGE  = WORKDIR + "package.tgz"
+	TEMPLATE = WORKDIR + "templated"
+	WORKDIR  = "/src/"
 )
 
 type Helm struct {
-	// +private
-	Main *MikaelElkiaer
+	// Base container with tools
+	Base *Container
 }
 
-// Helm package manager
+// Submodule for Helm
 func (m *MikaelElkiaer) Helm(
 	ctx context.Context,
-) *Helm {
-	return &Helm{Main: m}
-}
-
-type HelmPackage struct {
-	Container *Container
-	Parent    *Helm
-}
-
-// Build Helm package
-func (m *Helm) Build(
-	ctx context.Context,
-	// +default=true
-	enableDocs bool,
-	// +default=true
-	enableLint bool,
-	// +default=true
-	enableSchema bool,
-	// +default=true
-	enableUnittest bool,
-	// Local directory containing source files
-	source *Directory,
-) (*HelmPackage, error) {
-	c, error := m.base(ctx)
+) (*Helm, error) {
+	b, error := m.helm(ctx)
 	if error != nil {
 		return nil, error
 	}
+	return &Helm{Base: b}, nil
+}
 
+type HelmBuild struct {
+	// Current state
+	Container *Container
+	// +private
+	Base *Container
+	// +private
+	Source *Directory
+}
+
+// Run build commands
+func (m *Helm) Build(
+	ctx context.Context,
+	// Directory containing source files
+	source *Directory,
+) (*HelmBuild, error) {
 	helmIgnoreFile, error := source.File(".helmignore").Contents(ctx)
 	if error != nil {
 		return nil, error
 	}
 	helmIgnore := strings.Split(helmIgnoreFile, "\n")
 
-	c = c.
-		WithWorkdir(workDir).
-		WithDirectory(".", source.Directory("/"), ContainerWithDirectoryOpts{Include: []string{"Chart.lock", "Chart.yaml"}}).
-		WithExec([]string{"sh", "-c", "touch Chart.lock && yq --indent 0 '.dependencies | map(select(.repository | test(\"^https?://\")) | [\"helm\", \"repo\", \"add\", .name, .repository] | join(\" \")) | .[]' ./Chart.lock | sh --;"}).
-		WithExec([]string{"helm", "dependency", "build"}).
+	b := m.Base.
+		WithDirectory(WORKDIR, source.Directory("/"), ContainerWithDirectoryOpts{Include: []string{"Chart.lock", "Chart.yaml"}}).
+		WithExec(inSh(`touch Chart.lock && yq --indent 0 '.dependencies | map(select(.repository | test("^https?://")) | ["helm", "repo", "add", .name, .repository] | join(" ")) | .[]' ./Chart.lock | sh --;`)).
+		WithExec(inSh(`helm dependency build`)).
 		WithDirectory(".", source, ContainerWithDirectoryOpts{Exclude: helmIgnore})
 
-	if enableLint {
-		c = c.WithExec([]string{"helm", "lint"})
-	}
+	c := dag.Container().
+		WithDirectory(WORKDIR, b.Directory(WORKDIR))
 
-	chartType, error := c.WithExec([]string{"sh", "-c", "yq '.type' Chart.yaml"}).Stdout(ctx)
+	return &HelmBuild{Base: m.Base, Container: c, Source: source}, nil
+}
+
+// Get directory containing modified source files
+func (m *HelmBuild) AsDirectory(
+	ctx context.Context,
+) *Directory {
+	return m.Container.Directory(WORKDIR)
+}
+
+// Run helm lint
+func (m *HelmBuild) Lint(
+	ctx context.Context,
+) *HelmBuild {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.WithExec(inSh(`helm lint`))
+	}, nil)
+}
+
+// Run helm-schema (from @socialgouv)
+func (m *HelmBuild) Schema(
+	ctx context.Context,
+) *HelmBuild {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`helm-schema`))
+	}, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`npm install -g @socialgouv/helm-schema`))
+	})
+}
+
+// Run helm-docs (from @norwoodj)
+func (m *HelmBuild) Docs(
+	ctx context.Context,
+) *HelmBuild {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`/root/go/bin/helm-docs`))
+	}, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`go install github.com/norwoodj/helm-docs/cmd/helm-docs@latest`))
+	})
+}
+
+// Run helm-unittest (from @helm-unittest)
+func (m *HelmBuild) Unittest(
+	ctx context.Context,
+) *HelmBuild {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithDirectory(".", m.Source, ContainerWithDirectoryOpts{Include: []string{"**/*_test.yaml"}}).
+			WithExec(inSh(`helm unittest .`))
+	}, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`helm plugin install https://github.com/helm-unittest/helm-unittest.git`))
+	})
+}
+
+// Run all checks
+func (m *HelmBuild) Check(
+	ctx context.Context,
+) (*HelmBuild, error) {
+	chartType, error := m.Base.
+		WithDirectory(WORKDIR, m.Container.Directory(WORKDIR)).
+		WithExec(inSh(`yq '.type' Chart.yaml`)).Stdout(ctx)
 	if error != nil {
 		return nil, error
 	}
 
-	if strings.TrimSpace(chartType) != "library" {
-		if enableSchema {
-			c = c.WithExec([]string{"helm-schema"})
-		}
-
-		if enableDocs {
-			c = c.WithExec([]string{"/root/go/bin/helm-docs"})
-		}
-
-		if enableUnittest {
-			c = c.
-				WithDirectory(".", source, ContainerWithDirectoryOpts{Include: []string{"**/*_test.yaml"}}).
-				WithExec([]string{"helm", "unittest", "."})
-		}
+	if chartType == "library" {
+		return m, nil
 	}
 
-	c = c.WithExec([]string{"sh", "-c", "rm -f *.tgz"}).
-		WithExec([]string{"helm", "package", "."}).
-		WithExec([]string{"sh", "-c", "mv *.tgz package.tgz"})
-
-	return &HelmPackage{Container: c, Parent: m}, nil
+	return m.Lint(ctx).
+		Schema(ctx).
+		Docs(ctx).
+		Unittest(ctx), nil
 }
 
-func (hp *HelmPackage) Directory(
+// Package Helm chart
+func (m *HelmBuild) Package(
 	ctx context.Context,
-	// +default=["Chart.lock", "Chart.yaml", "package.tgz", "README.md", "values.schema.json"]
-	// Files to include when exporting
-	include []string,
-) (*Directory, error) {
-	c := dag.Container().
-		WithDirectory("/src", hp.Container.Directory(workDir), ContainerWithDirectoryOpts{Include: include}).
-		Directory("/src")
+) *HelmPackage {
+	b := m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`helm package .`)).
+			WithExec(inSh(`mv *.tgz %s`, PACKAGE))
+	}, nil)
 
-	return c, nil
+	return &HelmPackage{Base: m.Base, Container: b.Container}
 }
 
-func (hp *HelmPackage) List(
-	ctx context.Context,
-) (string, error) {
-	return hp.Container.WithExec([]string{"ls", workDir}).Stdout(ctx)
-}
-
-func (hp *HelmPackage) File(
-	ctx context.Context,
-) (*File, error) {
-	return hp.Container.File("package.tgz"), nil
-}
-
-func (hp *HelmPackage) Noop() {
-}
-
-func (hp *HelmPackage) Push(
-	ctx context.Context,
-	// Registry URI to push the Helm package
-	registry string,
-) error {
-	hp.Container.
-		WithExec([]string{"helm", "push", "package.tgz", registry})
-
-	return nil
-}
-
-func (hp *HelmPackage) Template(
+// Template Helm chart using source
+func (m *HelmBuild) Template(
 	ctx context.Context,
 	// Additional arguments to pass to helm template
 	// +default=""
 	additionalArgs string,
-	// Namespace to template
-	// +default="test"
-	namespace string,
-) (*HelmTemplated, error) {
-	c := hp.Container.
-		WithExec(inSh("helm template . --namespace=%s --output-dir=%s %s", namespace, templatedDir, additionalArgs))
-
-	return &HelmTemplated{Container: c, Parent: hp}, nil
+) *HelmTemplate {
+	return template(m.Base, m.Container, ".", additionalArgs)
 }
 
-func (hp *HelmPackage) Test(
+func (m *HelmBuild) run(
+	ctx context.Context,
+	execute func(*Container) *Container,
+	setup func(*Container) *Container,
+) *HelmBuild {
+	b := m.Base
+	if setup != nil {
+		b = setup(b)
+	}
+	b = b.WithDirectory(WORKDIR, m.Container.Directory(WORKDIR))
+	b = execute(b)
+	c := dag.Container().
+		WithDirectory(WORKDIR, b.Directory(WORKDIR))
+	return &HelmBuild{
+		Container: c,
+		Base:      m.Base,
+		Source:    m.Source,
+	}
+}
+
+func template(base *Container, container *Container, path string, additionalArgs string) *HelmTemplate {
+	b := &HelmBuild{Base: base, Container: container}
+	b = b.run(context.Background(), func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`helm template %s --output-dir=%s %s`, path, TEMPLATE, additionalArgs))
+	}, nil)
+	return &HelmTemplate{Base: base, Container: b.Container}
+}
+
+type HelmPackage struct {
+	// Current state
+	Container *Container
+	// +private
+	Base *Container
+}
+
+// Get Helm package
+func (m *HelmPackage) AsFile(
+	ctx context.Context,
+) *File {
+	return m.Container.File(PACKAGE)
+}
+
+// Push Helm package to registry
+func (m *HelmPackage) Push(
+	ctx context.Context,
+	// Registry URI to push the Helm package
+	registry string,
+) *HelmPackage {
+	c := m.Base.
+		WithDirectory(WORKDIR, m.Container.Directory(WORKDIR)).
+		WithExec(inSh(`helm push %s`, PACKAGE, registry))
+
+	return &HelmPackage{Base: m.Base, Container: c}
+}
+
+// Template Helm chart using package
+func (m *HelmPackage) Template(
+	ctx context.Context,
+	// Additional arguments to pass to helm template
+	// +default=""
+	additionalArgs string,
+) *HelmTemplate {
+	return template(m.Base, m.Container, PACKAGE, additionalArgs)
+}
+
+// Deploy Helm package to a cluster
+func (m *HelmPackage) Deploy(
 	ctx context.Context,
 	// Additional arguments to pass to helm upgrade
 	// +default=""
@@ -165,77 +246,116 @@ func (hp *HelmPackage) Test(
 	name string,
 	// Namespace of the Helm release
 	namespace string,
-) *Container {
-	c := hp.Container.
+) (*HelmPackage, error) {
+	c := m.Base.
+		WithFile(PACKAGE, m.AsFile(ctx)).
 		WithServiceBinding("kubernetes", kubernetesService).
 		WithFile("/root/.kube/config", kubeconfig).
-		WithExec([]string{"kubectl", "config", "set-cluster", "minikube", fmt.Sprintf("--server=https://kubernetes:%d", kubernetesPort)}).
-		WithExec(inSh("kubectl create namespace %s --dry-run=client --output=json | kubectl apply -f -", namespace))
+		WithExec(inSh(`kubectl config set-cluster minikube --server=https://kubernetes:%d`, kubernetesPort)).
+		WithExec(inSh(`kubectl create namespace %s --dry-run=client --output=json | kubectl apply -f -`, namespace))
 
-	c = createSecrets(c, hp.Parent.Main.Creds, name, namespace)
+	c = withDockerPullSecrets(c, name, namespace)
 
-	c = c.WithExec([]string{"sh", "-c", fmt.Sprintf("helm upgrade %s ./package.tgz --atomic --install --namespace %s --wait %s", name, namespace, additionalArgs)}).
-		WithExec([]string{"sh", "-c", fmt.Sprintf("helm uninstall %s --namespace %s --wait", name, namespace)}).
-		WithExec([]string{"sh", "-c", fmt.Sprintf("kubectl delete namespace %s", namespace)})
+	c = c.WithExec(inSh(`helm upgrade %s %s --atomic --install --namespace %s --wait %s`, name, PACKAGE, namespace, additionalArgs)).
+		WithExec(inSh(`helm uninstall %s --namespace %s --wait`, name, namespace)).
+		WithExec(inSh(`kubectl delete namespace %s`, namespace))
 
-	return c
+	return &HelmPackage{Base: m.Base, Container: m.Container}, nil
 }
 
-type HelmTemplated struct {
+type HelmTemplate struct {
+	// Current state
 	Container *Container
-	Parent    *HelmPackage
+	// +private
+	Base *Container
 }
 
-func (ht *HelmTemplated) Check(
+// Run kubectl-validate
+func (m *HelmTemplate) Validate(
 	ctx context.Context,
 	// Kubernetes version to check against
 	// +default="1.29"
 	kubernetesVersion string,
-) (*HelmTemplated, error) {
-	c := ht.Container.
-		WithExec(inSh("/root/go/bin/kubectl-validate %s --version %s", templatedDir, kubernetesVersion)).
-		WithExec(inSh("pluto detect-files --target-versions k8s=v%s --v 7 --directory %s", kubernetesVersion, templatedDir))
-
-	return &HelmTemplated{Container: c, Parent: ht.Parent}, nil
+) *HelmTemplate {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`/root/go/bin/kubectl-validate %s --version %s`, TEMPLATE, kubernetesVersion))
+	}, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`go install sigs.k8s.io/kubectl-validate@v0.0.4`))
+	})
 }
 
-func createSecrets(container *Container, creds []*Cred, name string, namespace string) *Container {
-	c := container
-	for _, cred := range creds {
-		c = c.
-			WithSecretVariable("__PASSWORD", cred.UserSecret).
-			WithEnvVariable("__USERNAME", cred.UserId).
-			WithExec(inSh(`kubectl create secret docker-registry %s-image-secret \
-				--namespace=%s \
-				--docker-server=ghcr.io \
-				--docker-username=$__USERNAME \
-				--docker-password=$__PASSWORD \
-				--dry-run=client --output=json | \
-			kubectl apply -f -`, name, namespace)).
-			WithoutSecretVariable("__PASSWORD").
-			WithoutEnvVariable("__USERNAME")
+// Run pluto (from @FairwindsOps)
+func (m *HelmTemplate) Pluto(
+	ctx context.Context,
+	// Kubernetes version to check against
+	// +default="1.29"
+	kubernetesVersion string,
+) *HelmTemplate {
+	return m.run(ctx, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`pluto detect-files --target-versions k8s=v%s --v 7 --directory %s`, kubernetesVersion, TEMPLATE))
+	}, func(c *Container) *Container {
+		return c.
+			WithExec(inSh(`wget https://github.com/FairwindsOps/pluto/releases/download/v5.19.4/pluto_5.19.4_linux_amd64.tar.gz -O pluto.tgz && tar -zxvf pluto.tgz pluto && mv pluto /usr/bin/pluto && rm pluto.tgz`))
+	})
+}
+
+// Run all checks
+func (m *HelmTemplate) Check(
+	ctx context.Context,
+	// Kubernetes version to check against
+	// +default="1.29"
+	kubernetesVersion string,
+) *HelmTemplate {
+	return m.
+		Validate(ctx, kubernetesVersion).
+		Pluto(ctx, kubernetesVersion)
+}
+
+// Package Helm chart
+func (m *HelmTemplate) Package(
+	ctx context.Context,
+) *HelmPackage {
+	b := &HelmBuild{Base: m.Base, Container: m.Container}
+	c := b.Package(ctx).Container
+
+	return &HelmPackage{Base: m.Base, Container: c}
+}
+
+func (m *HelmTemplate) run(
+	ctx context.Context,
+	execute func(*Container) *Container,
+	setup func(*Container) *Container,
+) *HelmTemplate {
+	b := m.Base
+	if setup != nil {
+		b = setup(b)
 	}
-	return c
+	b = b.WithDirectory(WORKDIR, m.Container.Directory(WORKDIR))
+	b = execute(b)
+	c := dag.Container().
+		WithDirectory(WORKDIR, b.Directory(WORKDIR))
+	return &HelmTemplate{
+		Base:      m.Base,
+		Container: c,
+	}
 }
 
-func (m *Helm) base(
+func (m *MikaelElkiaer) helm(
 	ctx context.Context,
 ) (*Container, error) {
 	c := dag.Container().
 		// TODO: Actually implement function to update the version
 		// @version policy=^3.0.0 resolved=3.19.1
 		From("docker.io/library/alpine@sha256:c5b1261d6d3e43071626931fc004f70149baeba2c8ec672bd4f27761f8e1ad6b").
-		WithExec([]string{"sh", "-c", "echo '@community https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories"}).
-		WithExec([]string{"apk", "add", "go@community=1.22.3-r0"}).
-		WithExec([]string{"apk", "add", "git=2.43.4-r0", "kubectl@community=1.30.0-r1", "helm=3.14.2-r2", "npm=10.2.5-r0", "yq=4.35.2-r4"}).
-		WithExec([]string{"go", "install", "github.com/norwoodj/helm-docs/cmd/helm-docs@latest"}).
-		WithExec([]string{"npm", "install", "-g", "@socialgouv/helm-schema"}).
-		WithExec([]string{"helm", "plugin", "install", "https://github.com/helm-unittest/helm-unittest.git"}).
-		WithExec(inSh("go install sigs.k8s.io/kubectl-validate@v0.0.4")).
-		WithExec(inSh("wget https://github.com/FairwindsOps/pluto/releases/download/v5.19.4/pluto_5.19.4_linux_amd64.tar.gz -O pluto.tgz && tar -zxvf pluto.tgz pluto && mv pluto /usr/bin/pluto && rm pluto.tgz"))
+		WithExec(inSh(`echo '@community https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories`)).
+		WithExec(inSh(`apk add go@community=1.22.3-r0`)).
+		WithExec(inSh(`apk add git=2.43.4-r0 kubectl@community=1.30.0-r1 helm=3.14.2-r2 npm=10.2.5-r0 yq=4.35.2-r4`))
 
-	if len(m.Main.AdditionalCAs) > 0 {
-		for _, ca := range m.Main.AdditionalCAs {
+	if len(m.AdditionalCAs) > 0 {
+		for _, ca := range m.AdditionalCAs {
 			name, error := ca.Name(ctx)
 			if error != nil {
 				return nil, error
@@ -244,15 +364,36 @@ func (m *Helm) base(
 				WithWorkdir("/usr/local/share/ca-certificates/").
 				WithMountedSecret(name, ca)
 		}
-		c = c.WithExec([]string{"update-ca-certificates"})
+		c = c.WithExec(inSh(`update-ca-certificates`))
 	}
 
-	for _, cred := range m.Main.Creds {
+	for _, cred := range m.Creds {
 		c = c.
 			WithSecretVariable("GH_TOKEN", cred.UserSecret).
-			WithExec([]string{"sh", "-c", fmt.Sprintf("echo $GH_TOKEN | helm registry login --username %s --password-stdin ghcr.io", cred.UserId)}).
+			WithExec(inSh(`echo $GH_TOKEN | helm registry login --username %s --password-stdin ghcr.io`, cred.UserId)).
 			WithoutSecretVariable("GH_TOKEN")
 	}
 
+	c = c.WithWorkdir(WORKDIR)
+
 	return c, nil
+}
+
+func withDockerPullSecrets(
+	container *Container,
+	name string,
+	namespace string,
+) *Container {
+	return container.
+		WithExec(inSh(`[ -f /root/.docker/config.json ] && cat <<EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: %s
+  namespace: %s
+data:
+  .dockerconfigjson: $(cat /root/.docker/config.json | base64 -w 0)
+type: kubernetes.io/dockerconfigjson
+EOF`, name, namespace))
 }
